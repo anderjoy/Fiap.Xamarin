c7949a18e9a756744b46285bd20ea5b335dbc9d6
logonrm <logonrm@fiap.com.br>
2018-05-17 23:13:01 -0300

    Exercicio MVVMBasic

---
+++ Code/XF.MVVMBasic/XF.MVVMBasic/XF.MVVMBasic/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using XF.MVVMBasic.ViewModel;
+
+namespace XF.MVVMBasic
+{
+	public partial class App : Application
+	{
+        public static AlunoViewModel alunoViewModel;
+
+		public App ()
+		{
+            if (alunoViewModel == null)
+            {
+                alunoViewModel = new AlunoViewModel();
+            }
+
+			InitializeComponent();
+
+			MainPage = new NavigationPage(new XF.MVVMBasic.View.AlunoView() { BindingContext = App.alunoViewModel });
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Code/XF.MVVMBasic/XF.MVVMBasic/XF.MVVMBasic/ViewModel/AlunoViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XF.MVVMBasic.Model;
+
+namespace XF.MVVMBasic.ViewModel
+{
+    public class AlunoViewModel
+    {
+        public Aluno AlunoModel { get; set; }
+
+        public ObservableCollection<Aluno> Alunos { get; private set; } = new ObservableCollection<Aluno>();
+        public Adicionar InserirAluno { get; set; }
+        public ICommand OnNovo { get; private set; }
+
+        public AlunoViewModel()
+        {
+            InserirAluno = new Adicionar(this);
+            OnNovo = new Command(Novo);
+
+        }
+
+        public async void AddAluno(Aluno param)
+        {
+            Alunos.Add(param);
+            await App.Current.MainPage.Navigation.PopAsync();
+        }
+
+        public async void Novo()
+        {
+            AlunoModel = new Aluno();
+            await App.Current.MainPage.Navigation.PushAsync(new View.NovoAlunoView() { BindingContext = App.alunoViewModel });
+        }
+
+    }
+
+    public class Adicionar : ICommand
+    {
+        AlunoViewModel alunoVM;
+        public Adicionar(AlunoViewModel paramVM )
+        {
+            alunoVM = paramVM;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            alunoVM.AddAluno(alunoVM.AlunoModel);
+        }
+    }
+}
