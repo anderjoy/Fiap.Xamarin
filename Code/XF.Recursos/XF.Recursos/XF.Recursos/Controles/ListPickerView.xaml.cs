a7c6a89e013d5ccea8a4e2c6d319705d67732e60
logonrm <logonrm@fiap.com.br>
2018-05-17 21:24:08 -0300

    Recursos

---
+++ Code/XF.Recursos/XF.Recursos/XF.Recursos/Controles/ListPickerView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Recursos.Controles
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListPickerView : ContentPage
+	{
+        private Dictionary<int, string> cores = new Dictionary<int, string>
+        {
+            { 1, "#008000" },
+            { 2, "#0000FF" },
+            { 3, "#FF0000" },
+            { 4, "#008000" },
+            { 5, "#0000FF" },
+            { 6, "#FF0000" },
+            { 7, "#008000" },
+            { 8, "#0000FF" },
+            { 9, "#FF0000" },
+            { 10, "#008000" },
+            { 11, "#0000FF" },
+            { 12, "#FF0000" },
+            { 13, "#FFFF00" }
+        };
+
+        public ListPickerView()
+        {
+            InitializeComponent();
+
+            foreach (var item in cores.Values)
+            {
+                pikCor.Items.Add(item);
+            }
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            boxCor.Color = Color.FromHex(pikCor.SelectedItem.ToString());
+        }
+    }
+}
---
+++ Code/XF.Recursos/XF.Recursos/XF.Recursos/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.Recursos
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        #region Controles
+        private async void btnEditor_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Controles.EditorView());
+        }
+
+        private async void btnPicker_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Controles.ListPickerView());
+        }
+
+        private async void btnData_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Controles.PickerView());
+        }
+
+        private async void btnProgresso_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Controles.ProgressoView());
+        }
+
+        private async void btnStepper_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Controles.StepperView());
+        }
+        #endregion
+    }
+}
